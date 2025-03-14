c9cc65ff304280514c6cb8bd31f59707739abd13
Abel Rafael <abel.rafael.roman@gmail.com>
2025-03-14 13:38:33 -0400

    Eliminacion de comentarios en el TestVaso

--- Maquina_de_café_2/Test_Maquina_De_Cafe/TestVaso.cs
+++ Maquina_de_café_2/Test_Maquina_De_Cafe/TestVaso.cs
@@ -8,7 +8,6 @@ namespace Test_Maquina_De_Cafe
         [Test]
         public void deberiaDevolverTrueSiHayVasos()
         {
-            //La idea es pasarle el tamaño del vaso, y preguntarle si hay de esos vasos
             Vaso vasoPequeño = new Vaso(10,2);
             bool result = vasoPequeño.hayVasosDisponibles(2);
             Assert.IsTrue(result);
@@ -17,7 +16,7 @@ namespace Test_Maquina_De_Cafe
         [Test]
         public void deberiaDevolverFalseSiNoHayVasos()
         {
-            //Lo mismo, pero aqui decimos que no hay vasos medianos
+            
             Vaso vasoMediano = new Vaso(0,2);
             bool result = vasoMediano.hayVasosDisponibles(2);
             Assert.IsFalse(result);
@@ -26,7 +25,7 @@ namespace Test_Maquina_De_Cafe
         [Test]
         public void deberiaRestarCantidadDeVasos()
         {
-            //La idea es que el método me reste 1 vaso, por cada vez que lo invoque
+           
             Vaso vasoGrande = new Vaso(10,3);
             int result = vasoGrande.restarVasos(3);
             Assert.AreEqual(9, result);
@@ -35,7 +34,7 @@ namespace Test_Maquina_De_Cafe
         [Test]
         public void deberiaDevolverFalseSiNoExisteEseVaso()
         {
-            //Si el tipo de vaso no existe, deberia retornar falso
+           
             Vaso vaso = new Vaso(20,1);
             bool result = vaso.hayVasosDisponibles(5);
             Assert.IsFalse(result);
